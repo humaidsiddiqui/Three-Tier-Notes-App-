85750930d97ffdc8a9c4a28f0d7f1858a9b1d527
Humaid Siddiqui <Humaidsiddiqui81@gmail.com>
2025-02-05 02:43:11 -0800

    index and styling done

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+notes = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# @app.route('/add', methods=['POST'])
+# def add_note():
+#     note = request.form.get('note')
+#     if note:
+#         notes.append(note)
+#     return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
